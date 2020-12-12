239db62b1ae03350336d54e3c823a0f1b7ded746
MaximilianKaindl <m.kaindl0208@gmail.com>
2020-12-12 23:11:19 +0100

    commit

--- SystemInfoSensor/PubSubClient.cs
+++ SystemInfoSensor/PubSubClient.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Google.Cloud.PubSub.V1;
-using Grpc.Core;
 
 namespace SystemInfoSensor
 {
